4f6daea1d92c1bdbb5cefab0628a3701055687ed
Jacek Gałka <jacgalka@icloud.com>
2019-08-15 22:11:58 +0200

    Styles and content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Popular products</h1>
       </header>
+      <main className="content" id="content">
+        <ul className="product-list">
+
+          <li className="product">
+            <img className="product-photo" src={logo} alt="Product"/>
+            <div className="product-details">
+              <div className="vote-container">
+                <img src={logo} alt="Arrow" className="up-vote"/>
+                <span className="votes">55</span>
+              </div>
+              <div className="product-info">
+                <h2 className="product-name">Haught or Naught</h2>
+                <p className="product-desc">
+                  High-minded or absent-minded? You decide.
+                </p>
+              </div>
+              <div className="submiter-container">
+                <small>Submitted by: </small>
+                <img src={logo} alt="Avatar" className="avatar"/>
+              </div>
+            </div>
+          </li>
+
+          <li className="product">
+            <img className="product-photo" src={logo} alt="Product"/>
+            <div className="product-details">
+              <div className="vote-container">
+                <img src={logo} alt="Arrow" className="up-vote"/>
+                <span className="votes">55</span>
+              </div>
+              <div className="product-info">
+                <h2 className="product-name">Haught or Naught</h2>
+                <p className="product-desc">
+                  High-minded or absent-minded? You decide.
+                </p>
+              </div>
+              <div className="submiter-container">
+                <small>Submitted by: </small>
+                <img src={logo} alt="Avatar" className="avatar"/>
+              </div>
+            </div>
+          </li>
+
+        </ul>
+      </main>
     </div>
   );
 }
